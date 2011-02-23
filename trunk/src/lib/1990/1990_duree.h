6208b4c9a6db3c71b350b81cc8a17bb75d24e027
bansan85 <bansan85@cf8e2dcc-3157-4e26-837b-d933b042d9ae>
2011-02-23 00:43:45 +0000

    Ajout des fonctions duree_projet* dans la librarie 1990

---
+++ trunk/src/lib/1990/1990_duree.h
@@ -0,0 +1,33 @@
+/*
+ * 2lgc_code : calcul de résistance des matériaux selon les normes Eurocodes
+ * Copyright (C) 2011
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// duree_projet_eu : renvoie la durée (en année) indicative de la norme européenne
+// Dans le cas où la durée indiquée par la norme européenne est un
+// intervalle d'année, la durée la plus importante est renvoyée.
+// Si la valeur de type ne correspond pas à une catégorie, la fonction renvoie -1.
+// type : categorie de durée d'utilisation de projet
+int duree_projet_eu(int type);
+// duree_projet_eu : renvoie la durée (en année) indicative de la norme française
+int duree_projet_fr(int type);
+
+// duree_projet_txt_eu : renvoie la description des catérogies des durées indicative de la norme européenne
+// Renvoie NULL si la catégorie n'existe pas
+// type : catégorie de durée d'utilisation de projet
+char* duree_projet_txt_eu(int type);
+// duree_projet_txt_fr : renvoie la description des catérogies des durées indicative de la norme française
+char* duree_projet_txt_fr(int type);
