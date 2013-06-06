463fcd0c6d62142b68afd5aead51118a9c0ebf15
bansan85 <bansan85@cf8e2dcc-3157-4e26-837b-d933b042d9ae>
2013-06-06 21:34:54 +0000

    [BRANCHE vues3d][NOUVEAUTE] : ajout de l'affichage selon les 3 faces restantes.

--- branch/vues3d/src/lib/common/common_projet.c
+++ branch/vues3d/src/lib/common/common_projet.c
@@ -223,6 +223,18 @@ gboolean projet_init_graphique(Projet *projet)
     gtk_menu_shell_append(GTK_MENU_SHELL(comps->menu_affichage_vues_list), comps->menu_affichage_yzx);
     g_signal_connect_swapped(comps->menu_affichage_yzx, "activate", G_CALLBACK(m3d_camera_axe_y_z_x), projet);
     
+    comps->menu_affichage_yz_x = gtk_menu_item_new_with_label(gettext("YZ vers X-"));
+    gtk_menu_shell_append(GTK_MENU_SHELL(comps->menu_affichage_vues_list), comps->menu_affichage_yz_x);
+    g_signal_connect_swapped(comps->menu_affichage_yz_x, "activate", G_CALLBACK(m3d_camera_axe_y_z__x), projet);
+    
+    comps->menu_affichage_xyz = gtk_menu_item_new_with_label(gettext("XY vers Z+"));
+    gtk_menu_shell_append(GTK_MENU_SHELL(comps->menu_affichage_vues_list), comps->menu_affichage_xyz);
+    g_signal_connect_swapped(comps->menu_affichage_xyz, "activate", G_CALLBACK(m3d_camera_axe_x_y_z), projet);
+    
+    comps->menu_affichage_xy_z = gtk_menu_item_new_with_label(gettext("XY vers Z-"));
+    gtk_menu_shell_append(GTK_MENU_SHELL(comps->menu_affichage_vues_list), comps->menu_affichage_xy_z);
+    g_signal_connect_swapped(comps->menu_affichage_xy_z, "activate", G_CALLBACK(m3d_camera_axe_x_y__z), projet);
+    
     comps->menu_modelisation_list = gtk_menu_new();
     comps->menu_modelisation = gtk_menu_item_new_with_label(gettext("Modélisation"));
     gtk_menu_shell_append(GTK_MENU_SHELL(comps->menu), comps->menu_modelisation);
