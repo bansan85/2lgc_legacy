cc102cdd946f1097027673c569b7b9d6e05409c9
bansan85 <bansan85@cf8e2dcc-3157-4e26-837b-d933b042d9ae>
2013-08-25 07:20:23 +0000

    [BUG] : Lors de la modification graphique d'une barre ayant une section personnalisée, la précédente forme n'était pas supprimée car aucun nom ne lui était affecté.

--- trunk/src/lib/common/common_m3d.cc
+++ trunk/src/lib/common/common_m3d.cc
@@ -1467,6 +1467,7 @@ gboolean m3d_barre(void *donnees_m3d, EF_Barre *barre)
                 list_parcours = g_list_next(list_parcours);
             }
             tout->rotations(90., 0., 0.);
+            tout->set_name(tmp);
             
             m3d_barre_finition(tout, barre);
             vue->scene->add_object(tout);
