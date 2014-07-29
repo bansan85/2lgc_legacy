a8e373f45fe9c61e086d7a391cbea01ac5227d87
LE GARREC Vincent <legarrec.vincent@gmail.com>
2014-07-29 23:27:29 +0000

    [TEST] : Ajout de la procédure de tests.

---
+++ src/test/scenarii/undomanager1.cpp
@@ -0,0 +1,110 @@
+/*
+ * 2lgc_code : calcul de résistance des matériaux selon les normes Eurocodes
+ * Copyright (C) 2011
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#include "codegui.hpp"
+#include "MErreurs.hh"
+
+#include <stdint.h>
+#include <libintl.h>
+#include <string.h>
+
+
+int
+main (int32_t argc,
+      char   *argv[])
+{
+  CProjet projet (NORME_EC);
+  std::unique_ptr <CAction> action;
+  
+  // On charge la localisation
+  setlocale (LC_ALL, "" );
+  bindtextdomain (PACKAGE, LOCALEDIR);
+  bind_textdomain_codeset (PACKAGE, "UTF-8");
+  textdomain (PACKAGE);
+  
+  // On traite les arguments du programme
+  switch (argc)
+  {
+    case 2:
+    {
+      if ((strcmp (argv[1], "-w") == 0) ||
+          (strcmp (argv[1], "--warranty") == 0))
+      {
+        CProjet::showWarranty ();
+        return 0;
+      }
+      else if ((strcmp (argv[1], "-h") == 0) ||
+               (strcmp (argv[1], "--help") == 0))
+      {
+        CProjet::showHelp ();
+        return 0;
+      }
+      break;
+    }
+    default:
+    {
+      break;
+    }
+  }
+  
+  BUGCONT (projet.getActionCount () == 0, -1, NULL)
+  // 0 Poids propre
+  action.reset (new CAction ("Poids propre",
+                             0,
+                             dynamic_cast <CUndoManager &> (projet)));
+  BUGCONT (projet.addAction (action.get ()), -1, NULL)
+  action.release ();
+  BUGCONT (projet.getActionCount () == 1, -1, NULL)
+  // 2 Exploitation
+  action.reset (new CAction ("Chargement",
+                             2,
+                             dynamic_cast <CUndoManager &> (projet)));
+  BUGCONT (projet.addAction (action.get ()), -1, NULL)
+  action.release ();
+  BUGCONT (projet.getActionCount () == 2, -1, NULL)
+  // 18 Neige
+  action.reset (new CAction ("Neige",
+                             18,
+                             dynamic_cast <CUndoManager &> (projet)));
+  BUGCONT (projet.addAction (action.get ()), -1, NULL)
+  action.release ();
+  BUGCONT (projet.getActionCount () == 3, -1, NULL)
+  // 19 Vent
+  action.reset (new CAction ("Vent",
+                             19,
+                             dynamic_cast <CUndoManager &> (projet)));
+  BUGCONT (projet.addAction (action.get ()), -1, NULL)
+  action.release ();
+  BUGCONT (projet.getActionCount () == 4, -1, NULL)
+  BUGCONT (projet.undo (), -1, NULL)
+  BUGCONT (projet.getActionCount () == 3, -1, NULL)
+  BUGCONT (projet.undo (), -1, NULL)
+  BUGCONT (projet.getActionCount () == 2, -1, NULL)
+  BUGCONT (projet.undo (), -1, NULL)
+  BUGCONT (projet.getActionCount () == 1, -1, NULL)
+  BUGCONT (projet.undo (), -1, NULL)
+  BUGCONT (projet.getActionCount () == 0, -1, NULL)
+  
+  BUGCONT (projet.enregistre ("test.xml"), -1, NULL)
+  
+  return 0;
+}
+
+/* vim:set shiftwidth=2 softtabstop=2 expandtab: */
