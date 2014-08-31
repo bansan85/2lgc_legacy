c5e4015a6f0caa075343e644d533caab327456be
LE GARREC Vincent <legarrec.vincent@gmail.com>
2014-08-31 19:15:24 +0000

    [CODE] : Regroupement de tous les CModele* dans CModele.

--- src/lib/CModele.cpp
+++ src/lib/CModele.cpp
@@ -20,18 +20,21 @@
 
 
 #include "CModele.hpp"
+#include "CNbCalcul.hpp"
+#include "CProjet.hpp"
+#include "MErreurs.hh"
 
 /**
  * \brief Initialise le modèle de calcul.
  */
 CModele::CModele () :
-  CModeleActions (),
-  CModeleBarres (),
-  CModeleRelachements (),
-  CModeleMateriaux (),
-  CModeleSections (),
-  CModeleNoeuds (),
-  CModeleAppuis (),
+  appuis (),
+  noeuds (),
+  sections (),
+  materiaux (),
+  relachements (),
+  barres (),
+  actions (),
   niveaux_groupes ()
 {
 }
@@ -59,6 +62,153 @@ CModele::~CModele ()
 {
   // Ce n'est pas modèle qui libère la mémoire, c'est le gestionnaire
   // d'annulation.
+}
+
+
+/**
+ * \brief Ajout d'une action. La fonction XML est CAction::addXML.
+ * \param action (in) L'action à ajouter.
+ * \return false en cas de problème.
+ */
+bool
+CModele::addAction (CAction * action)
+{
+  BUGPARAM (action, "%p", action, false, &action->getUndoManager ())
+  
+  BUGCRIT (actions.max_size () != actions.size (),
+           false,
+           &action->getUndoManager (),
+           gettext ("Le programme est à ses limites.\n"))
+  
+  BUGUSER (getAction (*action->getNom ()) == NULL,
+           false,
+           &action->getUndoManager (),
+           gettext ("L'action '%s' existe déjà.\nImpossible de l'ajouter.\n"),
+             action->getNom ()->c_str ())
+  
+  BUGPROG (action->emptyCharges (),
+           false,
+           &action->getUndoManager (),
+           gettext ("L'action doit être ajoutée sans charge. Elles doivent être ajoutées ensuite.\n"))
+  
+  BUGPROG (action->getType () < static_cast <CProjet *> (this)
+                                                ->getParametres ()->getpsiN (),
+           false,
+           &action->getUndoManager (),
+           gettext ("Le type d'action %d est inconnu.\n"), action->getType ())
+  
+  BUGCONT (action->getUndoManager ().ref (),
+           false,
+           &action->getUndoManager ())
+  
+  actions.push_back (action);
+  BUGCONT (action->getUndoManager ().push (
+             std::bind (static_cast <bool (CModele::*) (CAction *)>
+                                                          (&CModele::rmAction),
+                        this,
+                        action),
+             std::bind (&CModele::addAction, this, action),
+             std::bind (std::default_delete <CAction> (), action),
+             std::bind (&CAction::addXML,
+                        action,
+                        action->getNom (),
+                        action->getType (),
+                        std::placeholders::_1)),
+           false,
+           &action->getUndoManager ())
+  BUGCONT (action->getUndoManager ().pushSuppr (std::bind
+                    (std::default_delete <std::string> (), action->getNom ())),
+           false,
+           &action->getUndoManager ())
+  
+  IParametres *param = dynamic_cast <CProjet *> (this)->getParametres ();
+  uint8_t type = action->getType ();
+  uint8_t *decimales = dynamic_cast <CProjet *> (this)->getDecimales ();
+  
+  if (action->getUndoManager ().getEtat () != UNDO_NONE_OR_REVERT)
+  {
+    BUGCONT (action->setParam (
+               param,
+               new CNbCalcul (param->getpsi0 (type), U_, decimales),
+               new CNbCalcul (param->getpsi1 (type), U_, decimales),
+               new CNbCalcul (param->getpsi2 (type), U_, decimales)),
+             false,
+             &action->getUndoManager ())
+  }
+  
+  BUGCONT (action->getUndoManager ().unref (),
+           false,
+           &action->getUndoManager ())
+  
+  return true;
+}
+
+
+/**
+ * \brief Recherche une action.
+ * \param nom (in) Le nom de l'action.
+ */
+CAction *
+CModele::getAction (std::string nom)
+{
+  for (CAction * action : actions)
+  {
+    if (nom.compare (*action->getNom ()) == 0)
+    {
+      return action;
+    }
+  }
+  
+  return NULL;
+}
+
+
+/**
+ * \brief Renvoie le nombre d'actions.
+ */
+size_t
+CModele::getActionCount () const
+{
+  return actions.size ();
+}
+
+
+/**
+ * \brief Supprime d'une action.
+ * \param action (in) L'action à supprimer.
+ * \return false en cas de problème.
+ */
+bool
+CModele::rmAction (CAction * action)
+{
+  BUGPARAM (action, "%p", action, false, &action->getUndoManager ())
+  
+  BUGPROG (action->emptyCharges (),
+           false,
+           &action->getUndoManager (),
+           gettext ("L'action doit être supprimée sans charge. Elles doivent être supprimées avant.\n"))
+  
+  BUGCONT (action->getUndoManager ().ref (),
+           false,
+           &action->getUndoManager ())
+  
+  actions.remove (action);
+  BUGCONT (action->getUndoManager ().push (
+             std::bind (&CModele::addAction, this, action),
+             std::bind (static_cast <bool (CModele::*) (CAction *)>
+                                                          (&CModele::rmAction),
+                        this,
+                        action),
+             NULL,
+             NULL),
+           false,
+           &action->getUndoManager ())
+  
+  BUGCONT (action->getUndoManager ().unref (),
+           false,
+           &action->getUndoManager ());
+  
+  return true;
 }
 
 
