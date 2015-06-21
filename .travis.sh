7d2ea2f751fa6ddf7be5cd2a3451037d56ff4e75
bansan85 <legarrec.vincent@gmail.com>
2015-06-21 18:22:04 +0200

    [AUTOTOOLS] : Ajout de -fno-use-cxa-atexit via configure.ac.

--- .travis.sh
+++ .travis.sh
@@ -1,9 +1,8 @@
-gettextize -f --no-changelog && sed -i 's#po/Makefile.in po/Makefile.in#po/Makefile.in#g' configure.ac && sed -i 's/config.rpath  config.rpath/config.rpath/g' Makefile.am && cd po && patch < ../diff-po-Makevars.template.patch && cd .. && mv po/Makevars.template po/Makevars && rm -f po/POTFILES.in && find src/libeurocode -name "*.cpp" > po/POTFILES.in && find src/libundo -maxdepth 1 -name "*.cpp" >> po/POTFILES.in && autoheader && aclocal -I m4 && libtoolize && automake --add-missing --copy && autoconf
+gettextize -f --no-changelog && sed -i 's#po/Makefile.in po/Makefile.in#po/Makefile.in#g' configure.ac && sed -i 's/config.rpath  config.rpath/config.rpath/g' Makefile.am && cd po && patch < ../diff-po-Makevars.template.patch && cd .. && mv po/Makevars.template po/Makevars && rm -f po/POTFILES.in && find src/libeurocode -name "*.cpp" > po/POTFILES.in && find src/libundo -maxdepth 1 -name "*.cpp" >> po/POTFILES.in && autoheader && aclocal -I m4 && libtoolize && automake --add-missing --copy && autoconf || exit 1
+  ./configure --with-gui=none --with-debug --prefix=/usr/local CFLAGS="" CXXFLAGS="" && sed -i 's/-DPIC/-DPIC -Wl,--as-needed/g' libtool && find . -name "Makefile" -exec sed -i "s#-I/usr#-isystem/usr#g" {} \; || exit 1
 if [ "$CC" = "clang" ]; then
-  ./configure --with-gui=none --with-debug --prefix=/usr/local CFLAGS="" CXXFLAGS="-fno-use-cxa-atexit" && sed -i 's/-DPIC/-DPIC -Wl,--as-needed/g' libtool && find . -name "Makefile" -exec sed -i "s#-I/usr#-isystem/usr#g" {} \; && \
   scan-build make && scan-build make check && scan-build make distcheck || exit 1
 else
-  ./configure --with-gui=none --with-debug --prefix=/usr/local CFLAGS="" CXXFLAGS="" && sed -i 's/-DPIC/-DPIC -Wl,--as-needed/g' libtool && find . -name "Makefile" -exec sed -i "s#-I/usr#-isystem/usr#g" {} \;
-  cppcheck --inconclusive --enable=all src -I src/libprojet -I src/macro -I src/libeurocode /I src/libundo
+  cppcheck --inconclusive --enable=all src -I src/libprojet -I src/macro -I src/libeurocode -I src/libundo
   make && make check && make distcheck || exit 1
 fi
