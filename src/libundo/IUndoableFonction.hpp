7c839102d509681bcaf216c465b6d6c92ab15735
bansan85 <legarrec.vincent@gmail.com>
2015-07-02 22:18:06 +0200

    [CODE] : Création de l'interface IUndoableFonction.

---
+++ src/libundo/IUndoableFonction.hpp
@@ -0,0 +1,88 @@
+#ifndef IUNDOABLEFONCTION__HPP
+#define IUNDOABLEFONCTION__HPP
+
+/*
+2lgc_code : calcul de résistance des matériaux selon les normes Eurocodes
+Copyright (C) 2011
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Fichier généré automatiquement avec dia2code 0.9.0.
+ */
+
+#include <libxml/tree.h>
+
+class IUndoableFonction
+{
+  // Operations
+  public :
+    /**
+     * \brief Constructeur d'une interface IUndoableFonction.
+     */
+    IUndoableFonction ();
+    /**
+     * \brief Duplication d'une interface IUndoableFonction.
+     * \param other (in) La classe à dupliquer.
+     */
+    IUndoableFonction (const IUndoableFonction & other) = delete;
+    /**
+     * \brief Duplication d'une interface IUndoableFonction.
+     * \param other (in) La classe à dupliquer.
+     * \return IUndoableFonction &
+     */
+    IUndoableFonction & operator = (const IUndoableFonction & other) = delete;
+    /**
+     * \brief Destructeur d'une interface IUndoableFonction.
+     */
+    virtual ~IUndoableFonction ();
+    /**
+     * \brief Exécute la fonction.
+     * \return bool CHK
+     */
+    virtual bool CHK do ();
+    /**
+     * \brief Format XML pour appliquer les modifications.
+     * \param node (in) Le noeud où ajouter les informations.
+     * \return bool CHK
+     */
+    virtual bool CHK doXML (xmlNodePtr node);
+    /**
+     * \brief La description de la modification.
+     * \return const std::string &
+     */
+    virtual const std::string & doDescription ();
+    /**
+     * \brief Annule la fonction.
+     * \return bool CHK
+     */
+    virtual bool CHK undo ();
+    /**
+     * \brief Format XML  pour annuler les modifications.
+     * \param node (in) Le noeud où ajouter les informations.
+     * \return bool CHK
+     */
+    virtual bool CHK undoXML (xmlNodePtr node);
+    /**
+     * \brief La description de l'annulation.
+     * \return const std::string &
+     */
+    virtual const std::string & undoDescription ();
+    /**
+     * \brief La fonction renvoie true si la classe modifie le modèle et false si elle ne modifie rien de significatif.
+     * \return bool CHK
+     */
+    virtual bool CHK getModification ();
+};
+
+#endif
